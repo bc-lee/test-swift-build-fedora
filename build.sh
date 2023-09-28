1a5181faa8a165e12d12341ddd5c03cfa187a370
Byoungchan Lee <byoungchan.lee@gmx.com>
2023-09-28 11:04:01 +0900

    Add build script

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -exo pipefail
+
+dnf update -y
+dnf install -y which git rpm-build mock rpmdevtools
+
+mkdir -p /src
+cd /src
+git clone https://src.fedoraproject.org/rpms/swift-lang.git
+cd swift-lang
+git checkout b1cf890222c620e4be45ae54a48fb6c1ac52465a
+
+rm -rf $HOME/rpmbuild
+rpmdev-setuptree
+
+cp * $HOME/rpmbuild/SOURCES/
+mv $HOME/rpmbuild/SOURCES/*.spec $HOME/rpmbuild/SPECS/
+
+spectool -g -R  $HOME/rpmbuild/SPECS/swift-lang.spec
+
+dnf builddep -y $HOME/rpmbuild/SPECS/swift-lang.spec
+
+rpmbuild -ba $HOME/rpmbuild/SPECS/swift-lang.spec
